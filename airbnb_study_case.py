4b18dad4b32f80364992077b2f187d5cc0e93c6d
Alain Ramon Burkhard <114851831+AlainRBurkhard@users.noreply.github.com>
2023-03-13 00:04:31 +0100

    Update airbnb_study_case.py

--- airbnb_study_case.py
+++ airbnb_study_case.py
@@ -36,9 +36,6 @@ fig = go.Figure(data=[go.Pie(labels=md.index, values=md.values)])
 fig.update_layout(title='Distribution of Room Types')
 # top hosts
 
-top_host = df1.host_id.value_counts().head(10)
-top_host_df = pd.DataFrame({'Host_ID': top_host.index.astype(str),
-                            'Properties': top_host.values})
 
 top_host = df1.host_id.value_counts().head(10)
 top_host_df = pd.DataFrame({'Host_ID': top_host.index.astype(str),
@@ -48,7 +45,7 @@ fig1 = px.bar(top_host_df, x='Host_ID', y='Properties',
              color='Properties', color_continuous_scale='Reds')
 fig1.update_layout(title='Hosts with most properties in NYC', 
                   xaxis_title='Host ID', yaxis_title='Count of properties',
-                  xaxis_tickangle=-45)
+                  xaxis_tickangle=-45, xaxis_tickformat='%s')
 
 fig2 = px.histogram(df1.loc[df1['price']<1000,:], x='price', nbins=300, histnorm='', labels={'price':'Price'}, 
                     title='Distribution of Apartment Prices', 
